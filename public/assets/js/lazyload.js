1e6b3d880c0458135b80756238cb757ce42085ca
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-05-12 20:44:30 +0100

    Increase scrollThreshold, add silly little game

--- public/assets/js/lazyload.js
+++ public/assets/js/lazyload.js
@@ -4,7 +4,7 @@ var no_more_entries = $('.no-more-entries');
 
 var lazyloader = entries_container.lazyload({
     src: '/',
-    scrollThreshold: 0.6,
+    scrollThreshold: 0.9,
     loadStart: function() {
         load_more.activity({
             align: 'right',
@@ -39,5 +39,29 @@ var lazyloader = entries_container.lazyload({
 
 load_more.click(function(event) {
     lazyloader.load();
+    event.preventDefault();
+});
+
+var clicker = function() {
+
+    this.click_count = 0;
+    this.messages = {
+        10: "Seriously, there isn't anything else to load!",
+        20: "Okay, keep clicking. Maybe something will happen eventually...",
+        50: "That was a joke. Nothing is going to happen if you keep clicking.",
+        100: "Well, this has been fun but... I have to go now."
+    };
+
+};
+
+no_more_entries_clicker = new clicker();
+
+no_more_entries.click(function(event) {
+    no_more_entries_clicker.click_count++;
+
+    if (no_more_entries_clicker.click_count in no_more_entries_clicker.messages) {
+        $(this).text(no_more_entries_clicker.messages[no_more_entries_clicker.click_count]);
+    }
+
     event.preventDefault();
 });
