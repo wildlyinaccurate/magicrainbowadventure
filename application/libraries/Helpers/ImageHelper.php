5c9685304a9bd44b035fb9f998fe1a8fb697cd6e
Joseph Wynn <wildlyinaccurate@gmail.com>
2012-05-20 10:41:15 +0100

    Add ImageHelper

---
+++ application/libraries/Helpers/ImageHelper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Helpers;
+
+/**
+ * Image Helper Class
+ *
+ * Provides methods useful for processing images.
+ *
+ * @author  Joseph Wynn <joseph@wildlyinaccurate.com>
+ */
+class ImageHelper
+{
+
+	/**
+	 * Determine whether or not the given file is an animated GIF.
+	 *
+	 * @param	string	$file_path
+	 * @return	bool
+	 * @author  Frank <frank@huddler.com>
+	 * @link	http://www.php.net/manual/en/function.imagecreatefromgif.php#104473
+	 */
+	public static function isAnimatedGif($file_path)
+	{
+		if ( ! ($fh = @fopen($file_path, 'rb')))
+		{
+			return false;
+		}
+
+		$count = 0;
+
+		// An animated gif contains multiple "frames", with each frame having a
+		// header made up of:
+		//	* a static 4-byte sequence (\x00\x21\xF9\x04)
+		//	* 4 variable bytes
+		//	* a static 2-byte sequence (\x00\x2C) (some variants may use \x00\x21 ?)
+
+		// We read through the file til we reach the end of the file, or we've found
+		// at least 2 frame headers
+		while ( ! feof($fh) && $count < 2)
+		{
+			$chunk = fread($fh, 1024 * 100); //read 100kb at a time
+			$count += preg_match_all('#\x00\x21\xF9\x04.{4}\x00(\x2C|\x21)#s', $chunk, $matches);
+		}
+
+		fclose($fh);
+
+		return $count > 1;
+	}
+
+}
