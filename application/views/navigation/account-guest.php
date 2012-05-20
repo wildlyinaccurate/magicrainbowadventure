82a2405884aef2126a163c6e6bbf76acbafcea12
Joseph Wynn <wildlyinaccurate@gmail.com>
2012-05-20 10:24:23 +0100

    Switch Login/Signup nav items

--- application/views/navigation/account-guest.php
+++ application/views/navigation/account-guest.php
@@ -1,4 +1,4 @@
 <ul>
-	<li><?=HTML::link('account/login', Lang::line('general.log_in'))?></li>
 	<li><?=HTML::link('account/signup', Lang::line('general.sign_up'))?></li>
+	<li><?=HTML::link('account/login', Lang::line('general.log_in'))?></li>
 </ul>
