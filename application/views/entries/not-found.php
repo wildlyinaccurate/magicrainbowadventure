11f069e0fdd37c487133ce91ecd2d72a180d9ec9
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-05-28 20:39:07 +0100

    Style entries/not-found view

--- application/views/entries/not-found.php
+++ application/views/entries/not-found.php
@@ -1,1 +1,3 @@
-<p>That entry doesn't exist! It might have been removed, or maybe it was never here. Who knows.</p>
+<section class="entry">
+	<p>That entry doesn't exist! It might have been removed, or maybe it was never here. Who knows.</p>
+</section>
