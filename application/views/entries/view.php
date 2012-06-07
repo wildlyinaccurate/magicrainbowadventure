8036bd257855011b27c909fcef42b1f9b62a8afe
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-06-07 22:47:32 +0100

    Remove comments form

--- application/views/entries/view.php
+++ application/views/entries/view.php
@@ -6,17 +6,3 @@
 	<p class="description"><?=nl2br($entry->getDescription())?></p>
 	<p class="submitted-by">Submitted by <?=$entry->getUser()->getDisplayName()?> on <?=$entry->getCreatedDate()->format('d F Y')?></p>
 </section>
-
-<section class="comments">
-	<h3><?=Lang::line('entries.comments')?></h3>
-
-	<?php if ($entry->getComments()->count() > 0): ?>
-
-	<?php else: ?>
-		<p><?=Lang::line('entries.no_comments')?></p>
-	<?php endif; ?>
-
-	<?=View::make('entries/comments-form')->with(array(
-		'entry' => $entry,
-	))?>
-</section>
