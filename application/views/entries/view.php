7cc13493d23b889510701796c9fa04272533e6f7
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-06-09 00:26:07 +0100

    Add favourite button to large entry view

--- application/views/entries/view.php
+++ application/views/entries/view.php
@@ -3,6 +3,14 @@
 		<?=HTML::image($entry->getThumbnailUrl('xl'), $entry->getTitle())?>
 	</a>
 
+	<div class="buttons">
+		<?php $active = (Auth::check() && Auth::user()->getFavourites()->contains($entry)); ?>
+		<?=HTML::link("{$entry->getId()}/{$entry->getUrlTitle()}/favourite?favourite=" . (int) ! $active, $entry->getFavouritedBy()->count(), array(
+		'class' => ($active) ? 'favourite active' : 'favourite',
+		'title' => Lang::line('entries.button_favourite'),
+	))?>
+	</div>
+
 	<p class="description"><?=nl2br($entry->getDescription())?></p>
 	<p class="submitted-by">Submitted by <?=$entry->getUser()->getDisplayName()?> on <?=$entry->getCreatedDate()->format('d F Y')?></p>
 </section>
