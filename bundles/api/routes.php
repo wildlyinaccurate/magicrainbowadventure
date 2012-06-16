12c3a208586ecf304fc7fd9fc52ce1658c85b9b7
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-06-16 18:21:13 +0100

    Fix api/entries call

--- bundles/api/routes.php
+++ bundles/api/routes.php
@@ -23,7 +23,7 @@ Route::get('(:bundle)/entries/(:num?)', function($id = null) use ($entity_manage
 		$page = max(Input::get('page'), 1);
 		$per_page = min(Config::get('api::api.max_items_per_page'), Input::get('per_page', Config::get('api::api.default_items_per_page')));
 		$offset = $per_page * ($page - 1);
-		$entries = $entity_manager->getRepository('Entity\Entry')->getLatestEntries($offset, $per_page);
+		$entries = $entity_manager->getRepository('Entity\Entry')->getAllEntries($offset, $per_page);
 	}
 
 	$return_json = array();
