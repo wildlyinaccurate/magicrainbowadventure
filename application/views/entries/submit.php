82bc2e2a2c2cb42ee3ecc03169006ecb91e6c5fa
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-06-17 19:00:24 +0100

    Convert spaces to tabs

--- application/views/entries/submit.php
+++ application/views/entries/submit.php
@@ -1,42 +1,42 @@
 <section class="submit-entry">
 	<?=Form::open_for_files('entries/submit', 'POST', array('class' => 'form-horizontal')  )?>
 
-        <div class="control-group">
-            <?=Form::label('title', Lang::line('entries.label_title'), array('class' => 'control-label'))?>
+		<div class="control-group">
+			<?=Form::label('title', Lang::line('entries.label_title'), array('class' => 'control-label'))?>
 
-            <div class="controls">
-                <?=Form::text('title', Input::old('title'), array('maxlength' => 140))?>
-                <?=$errors->first('title', '<span class="error">:message</span>')?>
-            </div>
-        </div>
+			<div class="controls">
+				<?=Form::text('title', Input::old('title'), array('maxlength' => 140))?>
+				<?=$errors->first('title', '<span class="error">:message</span>')?>
+			</div>
+		</div>
 
-        <div class="control-group">
-            <?=Form::label('entry_image', Lang::line('entries.label_description'), array('class' => 'control-label'))?>
+		<div class="control-group">
+			<?=Form::label('entry_image', Lang::line('entries.label_description'), array('class' => 'control-label'))?>
 
-            <div class="controls">
-                <?=Form::textarea('description', Input::old('description'))?>
-                <?=$errors->first('description', '<span class="error">:message</span>')?>
-            </div>
-        </div>
+			<div class="controls">
+				<?=Form::textarea('description', Input::old('description'))?>
+				<?=$errors->first('description', '<span class="error">:message</span>')?>
+			</div>
+		</div>
 
-        <div class="control-group">
-            <?=Form::label('entry_image', Lang::line('entries.label_upload_file'), array('class' => 'control-label'))?>
+		<div class="control-group">
+			<?=Form::label('entry_image', Lang::line('entries.label_upload_file'), array('class' => 'control-label'))?>
 
-            <div class="controls">
-                <?=Form::file('entry_image')?>
-                <?=$errors->first('entry_image', '<span class="error">:message</span>')?>
-                <span class="help-block">Images must be in GIF or JPEG format and smaller than <?=round($max_upload_size / 1024)?>MB</>
-            </div>
-        </div>
+			<div class="controls">
+				<?=Form::file('entry_image')?>
+				<?=$errors->first('entry_image', '<span class="error">:message</span>')?>
+				<span class="help-block">Images must be in GIF or JPEG format and smaller than <?=round($max_upload_size / 1024)?>MB</>
+			</div>
+		</div>
 
-        <div class="control-group">
-            <?=Form::label('image_url', Lang::line('entries.label_image_url'), array('class' => 'control-label'))?>
+		<div class="control-group">
+			<?=Form::label('image_url', Lang::line('entries.label_image_url'), array('class' => 'control-label'))?>
 
-            <div class="controls">
-                <?=Form::text('image_url', Input::old('image_url'))?>
-                <?=$errors->first('image_url', '<span class="error">:message</span>')?>
-            </div>
-        </div>
+			<div class="controls">
+				<?=Form::text('image_url', Input::old('image_url'))?>
+				<?=$errors->first('image_url', '<span class="error">:message</span>')?>
+			</div>
+		</div>
 
 		<div class="form-actions">
 			<button type="submit" name="submit_entry" class="btn btn-primary"><?=Lang::line('entries.label_submit')?></button>
