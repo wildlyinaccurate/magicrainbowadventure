d2d35d5c4ec5c6b8201df4762cc916e8351d64b7
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-06-17 15:28:43 +0100

    Implent GET users and POST entries

---
+++ bundles/api/language/en/response.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+
+	'entry_not_found' => 'The entry you requested does not exist.',
+	'user_not_found' => 'The user you requested does not exist.',
+
+);
--- bundles/api/routes.php
+++ bundles/api/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-use Bgy\Doctrine\EntitySerializer;
+use Bgy\Doctrine\EntitySerializer,
+	Monolog\Logger,
+	Monolog\Handler\RotatingFileHandler;
 
 /**
  * Magic Rainbow Adventure API Bundle
@@ -10,28 +12,48 @@ use Bgy\Doctrine\EntitySerializer;
  * @author  Joseph Wynn <joseph@wildlyinaccurate.com>
  */
 
-/** @var $entity_manager \Doctrine\ORM\EntityManager */
+$log = IoC::resolve('log.global');
 $entity_manager = IoC::resolve('doctrine::manager');
+
+/**
+ * Add a log handler for every api call
+ */
+Route::filter('push_api_logger', function()
+{
+	$log = IoC::resolve('log.global');
+	$log->pushHandler(new RotatingFileHandler(path('storage') . 'logs/' . 'magicrainbowadventure-api.log'), Logger::INFO);
+});
+
+Route::filter('pattern: api/*', 'push_api_logger');
 
 /**
  * Retrieve one or many entries
  */
-Route::get('(:bundle)/entries/(:num?)', function($id = null) use ($entity_manager)
+Route::get('(:bundle)/entries/(:num?)', function($id = null) use ($entity_manager, $log)
 {
 	$entity_serializer = new EntitySerializer($entity_manager, 1);
 
 	if ($id !== null)
 	{
-		$entries = array($entity_manager->find('Entity\Entry', $id));
-	}
-	else
-	{
-		$page = max(Input::get('page'), 1);
-		$per_page = min(Config::get('api::api.max_items_per_page'), Input::get('per_page', Config::get('api::api.default_items_per_page')));
-		$offset = $per_page * ($page - 1);
-		$entries = $entity_manager->getRepository('Entity\Entry')->getAllEntries($offset, $per_page);
+		// Retrieve only one entry
+		$entry = $entity_manager->find('Entity\Entry', $id);
+
+		if ($entry === null)
+		{
+			return Response::json(array(
+				'error' => Lang::line('api::response.entry_not_found')->get(),
+			), 404);
+		}
+
+		return Response::json($entity_serializer->toArray($entry));
 	}
 
+	// Retrieve an array of entries
+	$page = max(Input::get('page'), 1);
+	$per_page = min(Config::get('api::api.max_items_per_page'), Input::get('per_page', Config::get('api::api.default_items_per_page')));
+	$offset = $per_page * ($page - 1);
+
+	$entries = $entity_manager->getRepository('Entity\Entry')->getAllEntries($offset, $per_page);
 	$return_json = array();
 
 	foreach ($entries as $entry)
@@ -41,6 +63,85 @@ Route::get('(:bundle)/entries/(:num?)', function($id = null) use ($entity_manage
 
 		$return_json[] = $entry_array;
 	}
+
+	$log->addDebug('Returning ' . count($return_json) . ' results.');
+
+	return Response::json($return_json);
+});
+
+/**
+ * Create or update an entry
+ */
+Route::post('(:bundle)/entries/(:num?)', function($id = null) use ($entity_manager, $log)
+{
+	$entity_serializer = new EntitySerializer($entity_manager, 1);
+
+	if ($id === null)
+	{
+		// Create a new Entry
+		// @TODO
+		return Response::make(null, 501);
+	}
+
+	// Update an existing entry
+	$entry = $entity_manager->find('Entity\Entry', $id);
+
+	if ($entry === null)
+	{
+		return Response::json(array(
+			'error' => Lang::line('api::response.entry_not_found')->get(),
+		), 404);
+	}
+
+	$entry_data = json_decode(Input::get('entry'));
+
+	$moderated_by = $entity_manager->find('Entity\User', $entry_data->moderated_by->id);
+
+	$entry->setTitle($entry_data->title)
+		->setDescription($entry_data->description)
+		->setApproved($entry_data->approved)
+		->setModeratedBy($moderated_by);
+
+	$entity_manager->persist($entry);
+	$entity_manager->flush();
+
+	return Response::json($entity_serializer->toArray($entry));
+});
+
+/**
+ * Retrieve one or many users
+ */
+Route::get('(:bundle)/users/(:num?)', function($id = null) use ($entity_manager, $log)
+{
+	$entity_serializer = new EntitySerializer($entity_manager, 0);
+
+	if ($id !== null)
+	{
+		$user = $entity_manager->find('Entity\User', $id);
+
+		if ($user === null)
+		{
+			return Response::json(array(
+				'error' => Lang::line('api::response.user_not_found')->get(),
+			), 404);
+		}
+
+		return Response::json($entity_serializer->toArray($user));
+	}
+
+	$page = max(Input::get('page'), 1);
+	$per_page = min(Config::get('api::api.max_items_per_page'), Input::get('per_page', Config::get('api::api.default_items_per_page')));
+	$offset = $per_page * ($page - 1);
+
+	$users = $entity_manager->getRepository('Entity\User')->getAllUsers($offset, $per_page);
+	$return_json = array();
+
+	foreach ($users as $user)
+	{
+		$return_json[] = $entity_serializer->toArray($user);
+	}
+
+	$log->addDebug('Returning ' . count($return_json) . ' results.');
 
 	return Response::json($return_json);
 });
