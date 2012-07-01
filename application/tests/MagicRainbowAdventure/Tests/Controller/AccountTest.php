86b5aeb1de7cf601de1073bfe7effec818ed2157
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-01 23:35:05 +0100

    Add more account controller tests

--- application/tests/MagicRainbowAdventure/Tests/Controller/AccountTest.php
+++ application/tests/MagicRainbowAdventure/Tests/Controller/AccountTest.php
@@ -5,11 +5,52 @@ namespace MagicRainbowAdventure\Tests\Controller;
 class AccountTest extends \MagicRainbowAdventure\Tests\ControllerTestCase
 {
 
-	public function testSignup()
+	public function testSignupWithNoInput()
 	{
-		$response = $this->call('account@signup', array(), 'POST');
-		$this->assertInstanceOf('Laravel\\Response', $response);
+		$response = $this->post('account@signup', array());
+		$this->assertInstanceOf('Laravel\\Redirect', $response);
 		$this->assertEquals('302', $response->foundation->getStatusCode());
+	}
+
+	public function testSignupWithBadInput()
+	{
+		$response = $this->post('account@signup', array(
+			'username' => 'someusername',
+			'email' => 'notvalid',
+			'password' => 'passw0rd',
+		));
+
+		$this->assertInstanceOf('Laravel\\Redirect', $response);
+	}
+
+	/**
+	 * @depends testSignupWithBadInput
+	 */
+	public function testSignupWithBadInputHasErrors()
+	{
+		$session_errors = \Laravel\Session::instance()->get('errors')->all();
+		$this->assertNotEmpty($session_errors);
+	}
+
+	public function testSignupWithGoodInput()
+	{
+		$response = $this->post('account@signup', array(
+			'username' => 'validusername',
+			'email' => 'some@validemail.com',
+			'password' => 'passw0rd',
+			'password_confirm' => 'passw0rd',
+		));
+
+		$this->assertInstanceOf('Laravel\\Redirect', $response);
+	}
+
+	/**
+	 * @depends testSignupWithGoodInput
+	 */
+	public function testSignupWithGoodInputHasNoErrors()
+	{
+		$session_errors = \Laravel\Session::instance()->get('errors')->all();
+		$this->assertEmpty($session_errors);
 	}
 
 }
