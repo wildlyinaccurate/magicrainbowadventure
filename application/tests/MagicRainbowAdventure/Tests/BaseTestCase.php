5f6d9f1e08f60f2864d385b18c4cba8f59e91ce9
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-01 23:34:23 +0100

    Add mock entity manager (currently not working)

--- application/tests/MagicRainbowAdventure/Tests/BaseTestCase.php
+++ application/tests/MagicRainbowAdventure/Tests/BaseTestCase.php
@@ -3,7 +3,8 @@
 namespace MagicRainbowAdventure\Tests;
 
 use MagicRainbowAdventure\Tests\Mocks\DropboxApiMock,
-	MagicRainbowAdventure\Tests\Mocks\AuthDriverMock;
+	MagicRainbowAdventure\Tests\Mocks\AuthDriverMock,
+	Doctrine\Tests\Mocks\EntityManagerMock;
 
 /**
  * Magic Rainbow Adventure Base Test Case
@@ -24,6 +25,27 @@ abstract class BaseTestCase extends \PHPUnit_Framework_TestCase {
 
 		\Laravel\Session::load();
 		\Laravel\IoC::instance('dropbox::api', new DropboxApiMock);
+		\Laravel\IoC::instance('doctrine::manager', $this->_getEntityManager());
+	}
+
+	protected function _getEntityManager()
+	{
+		$config = \Laravel\IoC::resolve('doctrine::manager')->getConfiguration();
+
+		$config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
+		$config->setMetadataDriverImpl($config->newDefaultAnnotationDriver());
+		$config->setQueryCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
+		$config->setProxyDir(__DIR__ . '/Entity/Proxies');
+		$config->setProxyNamespace('MagicRainbowAdventure\Tests\Entity\Proxies');
+
+		$conn = \Doctrine\DBAL\DriverManager::getConnection(array(
+			'driverClass'  => 'Doctrine\Tests\Mocks\DriverMock',
+			'wrapperClass' => 'Doctrine\Tests\Mocks\ConnectionMock',
+			'user'         => 'john',
+			'password'     => 'wayne'
+		), $config);
+
+		return EntityManagerMock::create($conn, $config);
 	}
 
 }
