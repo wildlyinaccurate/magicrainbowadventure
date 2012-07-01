1b1fa62a3960ccb446af321581765f199a642a85
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-01 23:34:01 +0100

    Add post() method

--- application/tests/MagicRainbowAdventure/Tests/ControllerTestCase.php
+++ application/tests/MagicRainbowAdventure/Tests/ControllerTestCase.php
@@ -22,13 +22,29 @@ abstract class ControllerTestCase extends BaseTestCase
 	 * @return	\Laravel\Response
 	 * @author  Joseph Wynn <joseph@wildlyinaccurate.com>
 	 */
-	public function call($destination, $parameters, $method = 'GET')
+	public function call($destination, $parameters = array(), $method = 'GET')
 	{
 		\Laravel\Request::foundation()->server->add(array(
 			'REQUEST_METHOD' => $method,
 		));
 
 		return \Laravel\Routing\Controller::call('account@signup', $parameters);
+	}
+
+	/**
+	 * Make a POST request to a controller method
+	 *
+	 * @param	string	$destination
+	 * @param	array	$post_data
+	 * @param	array	$parameters
+	 * @return	\Laravel\Response
+	 * @author  Joseph Wynn <joseph@wildlyinaccurate.com>
+	 */
+	public function post($destination, $post_data, $parameters = array())
+	{
+		\Laravel\Request::foundation()->request->add($post_data);
+
+		return $this->call($destination, $parameters, 'POST');
 	}
 
 }
