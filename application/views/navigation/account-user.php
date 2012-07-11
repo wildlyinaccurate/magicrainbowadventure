e4b521a05f1465caf00b8ebd5c1a929c651e0b46
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-12 00:11:25 +0100

    Auto-hide long display names

--- application/views/navigation/account-user.php
+++ application/views/navigation/account-user.php
@@ -1,4 +1,4 @@
 <ul>
-	<li><?=HTML::link('account', Auth::user()->getDisplayName())?></li>
+	<li><?=HTML::link('account', Auth::user()->getDisplayName(), array('class' => 'display-name text-overflow'))?></li>
 	<li><?=HTML::link('account/logout', Lang::line('general.log_out'))?></li>
 </ul>
