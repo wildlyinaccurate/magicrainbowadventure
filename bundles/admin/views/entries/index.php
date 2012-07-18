2bc51985b9cde32fe0c877face8f08fe10ca7797
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-18 22:41:44 +0100

    Add accept/decline buttons and extra info to entry modal; Revise styling

--- bundles/admin/views/entries/index.php
+++ bundles/admin/views/entries/index.php
@@ -3,8 +3,8 @@
 		<thead>
 			<tr>
 				<th>&nbsp;</th>
-				<th><?=Lang::line('admin::entries.status')?></th>
 				<th><?=Lang::line('admin::entries.title')?></th>
+				<th><?=Lang::line('admin::entries.status')?></th>
 				<th><?=Lang::line('admin::entries.description')?></th>
 				<th><?=Lang::line('admin::entries.date_created')?></th>
 				<th><?=Lang::line('admin::entries.actions')?></th>
@@ -17,20 +17,20 @@
 						<img data-bind="attr: { src: thumbnail_url().thumbnail, title: '<?=Lang::line('admin::entries.original_size')?>' + originalSize() }" />
 					</a>
 				</td>
+				<td data-bind="text: title"></td>
 				<td class="status">
 					<span data-bind="if: moderated_by() !== null">
 						<span class="verb" data-bind="text: status"></span> by <a href="#" class="text-overflow" data-bind="text: moderated_by().getDisplayName(), click: moderated_by().edit"></a>
 					</span>
 					<span data-bind="visible: moderated_by() === null"><?=Lang::line('admin::entries.awaiting_moderation')?></span>
 				</td>
-				<td data-bind="text: title"></td>
 				<td data-bind="text: description"></td>
 				<td>
 					<span data-bind="text: created_date().date"></span> by <a href="#" class="text-overflow" data-bind="text: user().getDisplayName(), click: user().edit"></a>
 				</td>
 				<td class="actions">
-					<button class="btn btn-success" data-bind="visible: moderated_by() === null, click: toggleApproved"><?=Lang::line('admin::entries.approve')?></button>
-					<button class="btn btn-danger" data-bind="visible: moderated_by() === null, click: toggleApproved"><?=Lang::line('admin::entries.decline')?></button>
+					<button class="btn btn-success" data-bind="visible: moderated_by() === null, click: setApprovedAndSave.bind($data, true)"><?=Lang::line('admin::entries.approve')?></button>
+					<button class="btn btn-danger" data-bind="visible: moderated_by() === null, click: setApprovedAndSave.bind($data, false)"><?=Lang::line('admin::entries.decline')?></button>
 					<button class="btn" data-bind="click: edit"><?=Lang::line('admin::entries.edit')?></button>
 				</td>
 			</tr>
--- bundles/admin/views/partials/entry-info-modal.php
+++ bundles/admin/views/partials/entry-info-modal.php
@@ -1,7 +1,10 @@
 <div class="entry-info modal hide fade">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal">×</button>
-		<h3 data-bind="text: title"></h3>
+		<h3>
+			<span data-bind="text: title"></span>
+			<a class="view-link" data-bind="attr: { href: '/' + id() + '/' + url_title() }">&raquo; View</a>
+		</h3>
 	</div>
 
 	<form data-bind="attr: { class: status }">
@@ -24,22 +27,31 @@
 				</div>
 			</div>
 
-			<ul class="control-group">
-				<li>
-					Created on <span data-bind="text: created_date().date"></span>
-					by <a href="#" class="text-overflow" data-bind="text: user().getDisplayName(), click: user().edit"></a>
-				</li>
-
+			<ul class="meta control-group">
 				<li>
 					<span data-bind="if: moderated_by() !== null" class="status">
 						<span class="verb" data-bind="text: status"></span> by <a href="#" class="text-overflow" data-bind="text: moderated_by().getDisplayName(), click: moderated_by().edit"></a>
 					</span>
 					<span data-bind="visible: moderated_by() === null"><?=Lang::line('admin::entries.awaiting_moderation')?></span>
 				</li>
+
+				<li>
+					Created: <span data-bind="text: created_date().date"></span>
+					by <a href="#" class="text-overflow" data-bind="text: user().getDisplayName(), click: user().edit"></a>
+				</li>
+
+				<li>
+					Last modified: <span data-bind="text: modified_date().date"></span>
+				</li>
 			</ul>
 		</div>
 
 		<div class="modal-footer">
+			<div class="left-buttons">
+				<button class="btn btn-success" data-bind="attr: { disabled: (approved() == true) }, click: setApproved.bind($data, true)"><?=Lang::line('admin::entries.approve')?></button>
+				<button class="btn btn-danger" data-bind="attr: { disabled: (approved() == false) }, click: setApproved.bind($data, false)"><?=Lang::line('admin::entries.decline')?></button>
+			</div>
+
 			<button class="btn" data-dismiss="modal"><?=Lang::line('general.close')?></button>
 			<button class="btn btn-primary" data-bind="click: save" data-dismiss="modal"><?=Lang::line('admin::general.save')?></button>
 		</div>
