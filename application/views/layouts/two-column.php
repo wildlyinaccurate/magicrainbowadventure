fa719a2cfaa300f7d425e7f896cbc39d84c8c81e
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-18 23:13:14 +0100

    Align google ads

--- application/views/layouts/two-column.php
+++ application/views/layouts/two-column.php
@@ -4,15 +4,18 @@
 	</div> <!-- #main -->
 
 	<div id="sidebar" class="fourcol last">
-		<section>
+		<section class="advertisement">
 			<script type="text/javascript"><!--
-				google_ad_client = "ca-pub-5620145070273164";
-				/* Magic Rainbow Adventure Sidebar */
-				google_ad_slot = "8881360341";
-				google_ad_width = 300;
-				google_ad_height = 250;
-			//--></script>
-			<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
+			google_ad_client = "ca-pub-5620145070273164";
+			/* Magic Rainbow Adventure Sidebar */
+			google_ad_slot = "8881360341";
+			google_ad_width = 300;
+			google_ad_height = 250;
+			//-->
+			</script>
+			<script type="text/javascript"
+			src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+			</script>
 		</section>
 	</div>
 </div>
