8e5230553fce4edb3378878e564610ec95be64d5
Joseph Wynn <joseph@wildlyinaccurate.com>
2012-07-27 19:21:39 +0100

    Switch to asynchronous Google analytics

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -15,17 +15,6 @@
 	<link rel="stylesheet" type="text/css" media="all" href="<?=URL::base();?>/basset/magicrainbowadventure.css">
 
 	<script src="<?=URL::base();?>/assets/js/modernizr-2.5.3.min.js"></script>
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-25125418-2']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
 </head>
 
 <body class="<?=(Auth::check()) ? 'logged-in' : 'guest'?>">
@@ -85,6 +74,11 @@
 
 	<script type="text/javascript">
 		MagicRainbowAdventure.Queue.init();
+
+        var _gaq=[['_setAccount','UA-25125418-2'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
 	</script>
 
 </body>
